af7671be6ccc4f1dae90cfe70ef8f3c6e20c474a
Prabhjot Singh <pjsingh1359@gmail.com>
2020-10-08 14:25:19 -0700

    Updates

---
+++ precache-manifest.de59300452d0b725d98ade6af9384844.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc67db1ffea478be3818ec40d7e3a99e",
+    "url": "/fcc-quote-generator/index.html"
+  },
+  {
+    "revision": "161359b4f069f33c6a78",
+    "url": "/fcc-quote-generator/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "61a3c7cbf457dbeceb30",
+    "url": "/fcc-quote-generator/static/css/main.77967e15.chunk.css"
+  },
+  {
+    "revision": "161359b4f069f33c6a78",
+    "url": "/fcc-quote-generator/static/js/2.3584cc80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fcc-quote-generator/static/js/2.3584cc80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a3c7cbf457dbeceb30",
+    "url": "/fcc-quote-generator/static/js/main.97ae39fb.chunk.js"
+  },
+  {
+    "revision": "e1c216e52931c92b4c99",
+    "url": "/fcc-quote-generator/static/js/runtime-main.b94249f5.js"
+  }
+]);
