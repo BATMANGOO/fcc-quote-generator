aa19c468e729eb0540306cf6e171f43938585fe4
Prabhjot Singh <pjsingh1359@gmail.com>
2020-10-08 08:22:24 -0700

    added transition from one color to the next

--- src/containers/QuoteGenerator/QuoteGenerator.jsx
+++ src/containers/QuoteGenerator/QuoteGenerator.jsx
@@ -3,22 +3,32 @@ import './QuoteGenerator.css';
 
 const body = document.getElementById('root');
 
+
 const QuoteGenerator = props => {
-  // body.style.backgroundColor = ;
+  // const quoteTxt = document.querySelector('#text');
+  // console.log(quoteTxt);
+  
+  // Helper function to call twitter share.
   const shareQuote = event => {
     event.preventDefault();
     window.open(`${"https://twitter.com/intent/tweet?text="} "${props.selectedQuote.quote}" -${props.selectedQuote.author}`);
   }
+  //styles
   body.style.backgroundColor = props.pallet[props.colorIdx];
+  body.style.transition = '1.5s ease';
+  // quoteTxt.animate({opacity: 0}, 500, function() {
+  //   this.animate({opacity: 1}, 500);
+  // })
+
   return (
     <div id="quote-box">
       <div id="quote-container">
-        <div id='text' className='quote' style={{'color': props.pallet[props.colorIdx]}}>{props.selectedQuote.quote}</div>
-        <div id="author" className='author' style={{'color': props.pallet[props.colorIdx]}}>- {props.selectedQuote.author}</div>
+        <div id='text' className='quote' style={{'color': props.pallet[props.colorIdx], 'transition' : '1.5s ease'}}>{props.selectedQuote.quote}</div>
+        <div id="author" className='author' style={{'color': props.pallet[props.colorIdx], 'transition': '1.5s ease'}}>- {props.selectedQuote.author}</div>
       </div>
       <div id='buttons'>
-        <a  className='tweet' id='tweet-quote' href="twitter.com/intent/tweet" title="tweet this quote!" ><button style={{'backgroundColor': props.pallet[props.colorIdx]}} id='tweet-button' onClick={shareQuote} className='button tweet-button'>Tweet</button></a>
-      <button type="button" id="new-quote" style={{'backgroundColor': props.pallet[props.colorIdx]}} className="button new-quote" onClick={props.assignNewQuoteIndex}>New Quote</button>
+        <a  className='tweet' id='tweet-quote' href="twitter.com/intent/tweet" title="tweet this quote!" ><button style={{'backgroundColor': props.pallet[props.colorIdx], 'transition': '1.5s ease'}} id='tweet-button' onClick={shareQuote} className='button tweet-button'>Tweet</button></a>
+      <button type="button" id="new-quote" style={{'backgroundColor': props.pallet[props.colorIdx], 'transition': '1.5s ease'}} className="button new-quote" onClick={props.assignNewQuoteIndex}>New Quote</button>
       </div>
     </div>
   );
